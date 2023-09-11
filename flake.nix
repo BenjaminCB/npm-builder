57a69a2543db31e20a898cec0a7ce6aca0bceb0b
bcb <benjamincb@hotmail.dk>
2023-09-11 23:04:27 +0200

    fix syntax error

--- flake.nix
+++ flake.nix
@@ -2,6 +2,6 @@
   description = "A flake containing npm-builder a function to build npm packages with private registries";
 
   outputs = { self }: {
-    npm-builder = import ./npm-builder.nix
+    npm-builder = import ./npm-builder.nix;
   };
 }
