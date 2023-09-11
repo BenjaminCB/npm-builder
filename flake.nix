4d16550fe255013a57ec9f4f4cd097a2541e7d36
bcb <benjamincb@hotmail.dk>
2023-09-11 23:03:07 +0200

    add npm-builder

---
+++ flake.nix
@@ -0,0 +1,7 @@
+{
+  description = "A flake containing npm-builder a function to build npm packages with private registries";
+
+  outputs = { self }: {
+    npm-builder = import ./npm-builder.nix
+  };
+}
---
+++ npm-builder.nix
@@ -0,0 +1,52 @@
+{ pkgs, npmToken, src }:
+
+let
+    # Read the package-lock.json as a Nix attrset
+    packageLock = builtins.fromJSON (builtins.readFile (src + "/package-lock.json"));
+
+    # Create an array of all (meaningful) dependencies
+    deps =  builtins.attrValues (removeAttrs packageLock.packages [ "" ])
+         ++ builtins.attrValues (removeAttrs packageLock.dependencies [ "" ])
+    ;
+
+    # Turn each dependency into a fetchurl call
+    tarballs = map (p: pkgs.fetchurl {
+        url = p.resolved;
+        hash = p.integrity;
+        curlOpts = "-H @${pkgs.writeText "authorization.txt" "Authorization: Bearer ${npmToken}"}";
+    }) deps;
+
+    # Write a file with the list of tarballs
+    tarballsFile = pkgs.writeTextFile {
+        name = "tarballs";
+        text = builtins.concatStringsSep "\n" tarballs;
+    };
+in pkgs.stdenv.mkDerivation {
+    inherit (packageLock) name version;
+    inherit src;
+    buildInputs = with pkgs; [ nodejs nodePackages.typescript ];
+    buildPhase = ''
+        export HOME=$PWD/.home
+        export npm_config_cache=$PWD/.npm
+        mkdir -p $out
+        cd $out/
+        cp -r $src/. .
+
+        while read p1; read p2; read p3; read p4; read p5; read p6; read p7; read p8;
+        do
+            echo "caching $p1 $p2 $p3 $p4 $p5 $p6 $p7 $p8"
+            npm cache add "$p1" &
+            npm cache add "$p2" &
+            npm cache add "$p3" &
+            npm cache add "$p4" &
+            npm cache add "$p5" &
+            npm cache add "$p6" &
+            npm cache add "$p7" &
+            npm cache add "$p8" &
+            wait
+        done <${tarballsFile}
+
+        npm ci
+        tsc --build .
+    '';
+}
