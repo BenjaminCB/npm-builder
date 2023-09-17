b2467df09ff5f64654375666ba97caf1c2bfdce4
bcb <benjamincb@hotmail.dk>
2023-09-17 13:58:30 +0200

    should now cache all packages

--- npm-builder.nix
+++ npm-builder.nix
@@ -34,8 +34,10 @@ in pkgs.stdenv.mkDerivation {
         cd $out/
         cp -r $src/. .
 
+        i=0
         while read p1; read p2; read p3; read p4; read p5; read p6; read p7; read p8;
         do
+            i=$((i+8))
             echo "caching $p1 $p2 $p3 $p4 $p5 $p6 $p7 $p8"
             npm cache add "$p1" &
             npm cache add "$p2" &
@@ -46,6 +48,18 @@ in pkgs.stdenv.mkDerivation {
             npm cache add "$p7" &
             npm cache add "$p8" &
             wait
+        done <${tarballsFile}
+
+        j=0
+        while read p;
+        do
+            j=$((j+1))
+            if [ $j -le $i ]
+            then
+                continue
+            fi
+            echo "caching $p"
+            npm cache add "$p"
         done <${tarballsFile}
 
         npm ci
